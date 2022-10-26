6ce9d62222d98d98ccf0c7fe618bd15d528b71a5
Chathuranga19 <it20224370@my.sliit.lk>
2022-10-26 13:24:49 +0530

    first commit

---
+++ src/Pages/test/Test.jsx
@@ -0,0 +1,15 @@
+import React from 'react'
+
+const Test = () => {
+
+    return (
+        <div>
+            <h1>
+                Testing 1,2,3 Ranabunga
+            </h1>
+        </div>
+    )
+}
+
+export default Test;
+
