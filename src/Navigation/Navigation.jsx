259d4333b1c4f33965f603d71aef81565ab95888
yasiruranathunga <yasirusr99@gmail.com>
2022-10-27 23:06:48 +0530

    login

--- src/Navigation/Navigation.jsx
+++ src/Navigation/Navigation.jsx
@@ -5,13 +5,14 @@ import {
 } from "react-router-dom";
 import Evaluate from '../Pages/procurement_staff/evaluate';
 import Items from '../Pages/procurement_staff/items';
+import Login from '../Pages/procurement_staff/login';
 import RequestList from '../Pages/procurement_staff/request_list';
-import Test from '../Pages/test/Test';
+
 
 function Navigation() {
     return (
         <Routes>
-                <Route exact path='/' element={<Test />} />
+                <Route exact path='/' element={<Login />} />
                 <Route path="/request_list/" element={<RequestList/>}/>
                 <Route path="/evaluate/:id" element={<Evaluate/>}/>
                 <Route path="/items/:id" element={<Items/>}/>
