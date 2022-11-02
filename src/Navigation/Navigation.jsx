8725035158f1776f4249fbf5917fff429641f807
Chathuranga19 <it20224370@my.sliit.lk>
2022-11-02 17:38:57 +0530

    Commit developed supplier management components and common components

--- src/Navigation/Navigation.jsx
+++ src/Navigation/Navigation.jsx
@@ -4,13 +4,11 @@ import {
     Route,
 } from "react-router-dom";
 import ArrangeOrder from '../Pages/SupplierComponent/ArrangeOrderComponent';
-import Test from '../Pages/test/Test';
 import Container from '../Components/Container';
 
 function Navigation() {
     return (
         <Routes>
-                <Route exact path='/' element={<Test />} />
                 <Route exact path='/order/deliveryDetails' element={<Container data={<ArrangeOrder />} />} />
         </Routes>
     )
