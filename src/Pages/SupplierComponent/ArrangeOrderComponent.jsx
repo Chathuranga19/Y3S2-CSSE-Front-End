c21755dd7b5db5f5687da871b9262c2180c91256
Chathuranga19 <it20224370@my.sliit.lk>
2022-11-02 17:44:10 +0530

    Commit developed supplier management components and common components

--- src/Pages/SupplierComponent/ArrangeOrderComponent.jsx
+++ src/Pages/SupplierComponent/ArrangeOrderComponent.jsx
@@ -14,8 +14,8 @@ const ArrangeOrder = () => {
     const [DataList, setDataList] = useState([])
     const [currentPage, setCurrentPage] = useState(1);
     const [indexOfFirstItem, setindexOfFirstItem] = useState(0);
-    const [indexOfLastItem, setindexOfLastItem] = useState(3);
-    const [recordsPerPage] = useState(3);
+    const [indexOfLastItem, setindexOfLastItem] = useState(5);
+    const [recordsPerPage] = useState(5);
     const [retrievedData, setretrievedData] = useState([]);
     const [name, setName] = useState('');
     const [address, setAddress] = useState('');
@@ -289,7 +289,7 @@ const ArrangeOrder = () => {
                                     Accepted Order Details Unavailable !
                                 </span>
                             )}
-                            
+
                             <Pagination
                                 itemsCount={DataList.length}
                                 itemsPerPage={recordsPerPage}
